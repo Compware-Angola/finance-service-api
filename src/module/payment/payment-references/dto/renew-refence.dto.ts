cee4ee24b59189b8efa6e747d82b2f8dbf90d6a9
Isaac Isvaldo Bunga <isaacisvaldobunga300@gmail.com>
2025-10-30 17:02:25 +0100

    feat: update RenewReferenceDto to include newAmount with validation and Swagger documentation

--- src/module/payment/payment-references/dto/renew-refence.dto.ts
+++ src/module/payment/payment-references/dto/renew-refence.dto.ts
@@ -1,7 +1,16 @@
-import { IsOptional, IsString } from "class-validator";
+import { ApiPropertyOptional } from "@nestjs/swagger";
+import { IsOptional, IsNumber } from "class-validator";
+import { Type } from "class-transformer";
 
 export class RenewReferenceDto {
+  
+  @ApiPropertyOptional({
+    description: 'Novo valor da referência (opcional)',
+    example: 15000,   // exemplo mostrado no Swagger
+    type: Number,
+  })
   @IsOptional()
-  @IsString()
+  @IsNumber()
+  @Type(() => Number) // garante conversão para número mesmo vindo como string no body
   newAmount?: number;
 }

738816269e21dbf5d1d99b08a2ed3615fc5b5cd7
pedrok <pedrok@grupopanvel.com.br>
2026-01-28 21:28:05 -0300

    create and update

--- src/module/type_service/dto/create-type_service.dto.ts
+++ src/module/type_service/dto/create-type_service.dto.ts
@@ -1,1 +1,72 @@
-export class CreateTypeServiceDto {}
+import { IsNumber, IsOptional, IsString, IsBoolean } from 'class-validator';
+
+export class CreateTypeServiceDto {
+  @IsNumber()
+  taxaIvaId?: number;
+
+  @IsNumber()
+  motivoIsencaoIvaCodigo?: number;
+
+  @IsNumber()
+  preco: number;
+
+  @IsString()
+  descricao: string;
+
+  @IsOptional()
+  @IsString()
+  tipoServico: string;
+
+  @IsOptional()
+  @IsBoolean()
+  estado: boolean;
+
+  @IsOptional()
+  @IsString()
+  data?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  disponibilizarAluno?: boolean;
+
+  @IsOptional()
+  @IsNumber()
+  codigoGradeCurricular?: number;
+
+  @IsOptional()
+  @IsBoolean()
+  mestrado?: boolean;
+
+  @IsOptional()
+  @IsNumber()
+  canal?: number;
+
+  @IsNumber()
+  poloId?: number;
+
+  @IsOptional()
+  @IsBoolean()
+  cacuaco?: boolean;
+
+  @IsNumber()
+  codigoAnoLectivo: number;
+
+  @IsOptional()
+  @IsNumber()
+  valorAnterior?: number;
+
+  @IsOptional()
+  @IsBoolean()
+  visualizarNoPortal?: boolean;
+
+  @IsString()
+  sigla?: string;
+
+  @IsOptional()
+  @IsNumber()
+  estadoSolicitacao?: number;
+
+  @IsOptional()
+  @IsNumber()
+  tipoCandidatura?: number;
+}
--- src/module/type_service/dto/update-type_service.dto.ts
+++ src/module/type_service/dto/update-type_service.dto.ts
@@ -1,4 +1,11 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateTypeServiceDto } from './create-type_service.dto';
+import { IsOptional, IsNumber, IsString, IsBoolean } from 'class-validator';
 
-export class UpdateTypeServiceDto extends PartialType(CreateTypeServiceDto) {}
+export class UpdateTypeServiceDto {
+  @IsOptional() @IsNumber() taxaIvaId?: number;
+  @IsOptional() @IsNumber() motivoIsencaoIvaCodigo?: number;
+  @IsOptional() @IsNumber() poloId?: number;
+  @IsOptional() @IsNumber() codigoAnoLectivo?: number;
+  @IsOptional() @IsNumber() preco?: number;
+  @IsOptional() @IsString() descricao?: string;
+  @IsOptional() @IsBoolean() estado?: boolean;
+}

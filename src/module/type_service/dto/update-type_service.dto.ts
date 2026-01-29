c0fe5b1d8d1afb947e75f2705d5f064288aed787
Isaac Bunga <isaacisvaldobunga300@gmail.com>
2026-01-29 13:13:22 +0100

    feat: enhance DTOs with additional properties for type service

--- src/module/type_service/dto/update-type_service.dto.ts
+++ src/module/type_service/dto/update-type_service.dto.ts
@@ -1,11 +1,39 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsOptional, IsNumber, IsString, IsBoolean } from 'class-validator';
 
 export class UpdateTypeServiceDto {
-  @IsOptional() @IsNumber() taxaIvaId?: number;
-  @IsOptional() @IsNumber() motivoIsencaoIvaCodigo?: number;
-  @IsOptional() @IsNumber() poloId?: number;
-  @IsOptional() @IsNumber() codigoAnoLectivo?: number;
-  @IsOptional() @IsNumber() preco?: number;
-  @IsOptional() @IsString() descricao?: string;
-  @IsOptional() @IsBoolean() estado?: boolean;
+  @ApiPropertyOptional({ example: 1, description: 'ID da taxa de IVA' })
+  @IsOptional()
+  @IsNumber()
+  taxaIvaId?: number;
+
+  @ApiPropertyOptional({ example: 101, description: 'Código do motivo de isenção de IVA' })
+  @IsOptional()
+  @IsNumber()
+  motivoIsencaoIvaCodigo?: number;
+
+  @ApiPropertyOptional({ example: 3, description: 'ID do polo' })
+  @IsOptional()
+  @IsNumber()
+  poloId?: number;
+
+  @ApiPropertyOptional({ example: 2025, description: 'Código do ano lectivo' })
+  @IsOptional()
+  @IsNumber()
+  codigoAnoLectivo?: number;
+
+  @ApiPropertyOptional({ example: 28000, description: 'Novo preço do serviço' })
+  @IsOptional()
+  @IsNumber()
+  preco?: number;
+
+  @ApiPropertyOptional({ example: 'Propina Atualizada', description: 'Descrição do serviço' })
+  @IsOptional()
+  @IsString()
+  descricao?: string;
+
+  @ApiPropertyOptional({ example: true, description: 'Estado do serviço' })
+  @IsOptional()
+  @IsBoolean()
+  estado?: boolean;
 }

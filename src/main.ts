3259d922a48dcde018f3e93538c40360d9890c21
Isaac Bunga <isaacisvaldobunga300@gmail.com>
2026-02-05 16:54:26 +0100

    feat:update data

--- src/main.ts
+++ src/main.ts
@@ -39,7 +39,7 @@ async function bootstrap() {
   );
 
   // Porta
-  const port = process.env.PORT ?? 3002;
+  const port = process.env.PORT as any;
   await app.listen(port);
 
   console.log(`🚀 API Listening on port ${port}.`);

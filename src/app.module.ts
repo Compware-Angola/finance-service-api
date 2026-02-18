b98babcf07c1fc174ac9c8248ceb49a572b165c6
pedrok <pedrok@grupopanvel.com.br>
2026-02-18 15:18:30 -0300

    Revert "fix validation params"
    This reverts commit f179de4e3f80bf9c055f96f5f8df827c9876f3ec.

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,6 @@ import { HttpModule } from '@nestjs/axios';
 import { SharedModule } from './module/shared/shared.module';
 import { MonthlyFeePenaltyCronTest } from './module/jobs/payment-monthly-fee';
 import { DiscountModule } from './module/discount/discount.module';
-import { IsencaoModule } from './module/isencao/isencao.module';
 
 import { TipoCreditoModule } from './module/tipo_credito/tipo_credito.module';
 
@@ -94,7 +93,6 @@ import { TipoCreditoModule } from './module/tipo_credito/tipo_credito.module';
 
     SharedModule,
     DiscountModule,
-    IsencaoModule,
   ],
   providers: [BullMQWorkerService, MonthlyFeePenaltyCronTest],
 })

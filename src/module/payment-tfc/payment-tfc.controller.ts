f201d61970e9e7a1da83f1b1baf1a7c1feff63f6
nzinga74 <nzingaenoque@gmail.com>
2026-02-23 11:31:49 +0100

    Payment TFC

--- src/module/payment-tfc/payment-tfc.controller.ts
+++ src/module/payment-tfc/payment-tfc.controller.ts
@@ -1,34 +1,29 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { PaymentTfcService } from './payment-tfc.service';
 import { CreatePaymentTfcDto } from './dto/create-payment-tfc.dto';
 import { UpdatePaymentTfcDto } from './dto/update-payment-tfc.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { FindPaymentTFCDTO } from './dto/find-payment-tfc.dto';
 
 @Controller('payment-tfc')
 export class PaymentTfcController {
   constructor(private readonly paymentTfcService: PaymentTfcService) {}
-
-  @Post()
-  create(@Body() createPaymentTfcDto: CreatePaymentTfcDto) {
-    return this.paymentTfcService.create(createPaymentTfcDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.paymentTfcService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.paymentTfcService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePaymentTfcDto: UpdatePaymentTfcDto) {
-    return this.paymentTfcService.update(+id, updatePaymentTfcDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.paymentTfcService.remove(+id);
+  @Get('payments-tfc')
+  @ApiOperation({ summary: 'Lista de pagamentos TFC' })
+  @ApiResponse({
+    status: 200,
+    description: 'Lista de pagamentos TFC.',
+  })
+  async getStudentPayments(@Query() query: FindPaymentTFCDTO) {
+    return this.paymentTfcService.findPagamentosTFC(query);
   }
 }
--- src/module/payment-tfc/payment-tfc.service.ts
+++ src/module/payment-tfc/payment-tfc.service.ts
@@ -1,26 +1,87 @@
 import { Injectable } from '@nestjs/common';
-import { CreatePaymentTfcDto } from './dto/create-payment-tfc.dto';
-import { UpdatePaymentTfcDto } from './dto/update-payment-tfc.dto';
+import { DataSource } from 'typeorm';
+import { toLowerCaseKeys } from '../util/toLowerCaseKeys';
+import { FindPaymentTFCDTO } from './dto/find-payment-tfc.dto';
 
 @Injectable()
 export class PaymentTfcService {
-  create(createPaymentTfcDto: CreatePaymentTfcDto) {
-    return 'This action adds a new paymentTfc';
-  }
+  constructor(private dataSource: DataSource) {}
+  async findPagamentosTFC(filters: FindPaymentTFCDTO) {
+    const {
+      anoLectivo,
+      curso,
+      periodoId,
+      page = 1,
+      status,
+      limit = 25,
+    } = filters;
+    const offset = (page - 1) * limit;
 
-  findAll() {
-    return `This action returns all paymentTfc`;
-  }
+    let baseWhere = `
+    s.sigla = 'JdMdFdC'
+    AND f.ANO_LECTIVO = ${anoLectivo}
+  `;
 
-  findOne(id: number) {
-    return `This action returns a #${id} paymentTfc`;
-  }
+    if (curso) {
+      baseWhere += ` AND tm.Codigo_Curso = ${curso}`;
+    }
+    if (periodoId) {
+      baseWhere += ` AND tprd.codigo = ${periodoId}`;
+    }
 
-  update(id: number, updatePaymentTfcDto: UpdatePaymentTfcDto) {
-    return `This action updates a #${id} paymentTfc`;
-  }
+    //Estado Factura
+    if (status) {
+      baseWhere += ` AND f.estado = ${status}`;
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} paymentTfc`;
+    const sql = `
+    SELECT
+      tp.NOME_COMPLETO AS nome,
+      tm.codigo AS matricula,
+      p.codigo AS pagamento,
+      tc.designacao AS curso,
+      p.estado AS estado
+    FROM FK2_FACTURA f
+      LEFT JOIN FK2_TB_PAGAMENTOS p ON p.CODIGO_FACTURA = f.codigo
+      INNER JOIN FK2_FACTURA_ITEMS it ON it.CODIGOFACTURA = f.codigo
+      INNER JOIN FK2_TB_TIPO_SERVICOS s ON s.codigo = it.CODIGOPRODUTO
+      INNER JOIN FK2_TB_MATRICULAS tm ON tm.Codigo = f.CodigoMatricula
+      INNER JOIN FK2_TB_ADMISSAO ta ON ta.codigo = tm.Codigo_Aluno
+      INNER JOIN FK2_TB_PREINSCRICAO tp ON tp.Codigo = ta.pre_incricao
+      INNER JOIN FK2_TB_CURSOS tc ON tc.Codigo = tm.Codigo_Curso
+      INNER JOIN FK2_TB_PERIODOS tprd ON tprd.Codigo = tp.Codigo_Turno
+    WHERE ${baseWhere}
+    ORDER BY tm.codigo ASC
+    OFFSET ${offset} ROWS FETCH NEXT ${limit} ROWS ONLY
+  `;
+
+    const sqlCount = `
+    SELECT COUNT(*) AS TOTAL
+    FROM FK2_FACTURA f
+      LEFT JOIN FK2_TB_PAGAMENTOS p ON p.CODIGO_FACTURA = f.codigo
+      INNER JOIN FK2_FACTURA_ITEMS it ON it.CODIGOFACTURA = f.codigo
+      INNER JOIN FK2_TB_TIPO_SERVICOS s ON s.codigo = it.CODIGOPRODUTO
+      INNER JOIN FK2_TB_MATRICULAS tm ON tm.Codigo = f.CodigoMatricula
+      INNER JOIN FK2_TB_ADMISSAO ta ON ta.codigo = tm.Codigo_Aluno
+      INNER JOIN FK2_TB_PREINSCRICAO tp ON tp.Codigo = ta.pre_incricao
+      INNER JOIN FK2_TB_CURSOS tc ON tc.Codigo = tm.Codigo_Curso
+      INNER JOIN FK2_TB_PERIODOS tprd ON tprd.Codigo = tp.Codigo_Turno
+    WHERE ${baseWhere}
+  `;
+    const [result, countResult] = await Promise.all([
+      this.dataSource.query(sql),
+      this.dataSource.query(sqlCount),
+    ]);
+
+    const total = Number(countResult[0].TOTAL);
+    const totalPages = Math.ceil(total / limit);
+
+    return {
+      data: await toLowerCaseKeys(result),
+      total,
+      page,
+      limit,
+      totalPages,
+    };
   }
 }

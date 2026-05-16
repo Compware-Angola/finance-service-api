39d1a2b6a6c1238a9edc45b77c622837a6afe07e
Isaac Bunga <isaacisvaldobunga300@gmail.com>
2026-05-16 10:56:04 +0100

    feat: implement fee exemption check based on monthly status in monthly fees service

--- src/module/shared/monthly_fees/monthly_fees.discount.Util.service.ts
+++ src/module/shared/monthly_fees/monthly_fees.discount.Util.service.ts
@@ -387,15 +387,19 @@ export class MonthlyFeesDiscountUtilService {
     // Se for Bolseiro sem Multa nao deve calcular mas a Multa
 
     let percentagemMulta = 0;
-    /*
-    if (!bolseiroInfo.isentar_multa) {
+    const temMesesSemMulta = await this.obterMesesSemMulta(
+      mesTemp.ano_lectivo,
+      mesTemp.id,
+    );
+
+
+    if (!bolseiroInfo.isentar_multa && !temMesesSemMulta) {
       percentagemMulta = await this.calcularPercentagemMulta(
         codigoMatricula,
         mesTemp,
         periodosIsentos,
       );
     }
-    */
     const multa = mensalidadeComDesconto * percentagemMulta;
     const valorFinal = mensalidadeComDesconto + multa;
     const valorPago = isBolseiroIntegral ? mensalidade.preco : 0;
@@ -427,6 +431,24 @@ export class MonthlyFeesDiscountUtilService {
       data_operacao: null,
       data_pagamento: null,
     };
+  }
+
+  private async obterMesesSemMulta(anoLectivo: number, mesTempId: number): Promise<boolean> {
+    const sql = `
+    SELECT COUNT(*) AS TOTAL
+    FROM FK2_TB_MESES_SEM_MULTA TSM
+    WHERE TSM.ANO_LECTIVO = :anoLectivo
+      AND TSM.MES_TEMP_ID = :mesTempId
+      AND TSM.ESTADO = 1
+  `;
+
+    const result = await this.dataSource.query(sql, {
+      anoLectivo,
+      mesTempId,
+    } as any);
+
+    const total = Number(result[0]?.TOTAL ?? 0);
+    return total > 0;
   }
 
   // ====================== MÉTODO PRINCIPAL ======================

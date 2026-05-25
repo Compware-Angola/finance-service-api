160877f1ee090dfaf4ebbdf2f184518a054c7837
nzinga74 <nzingaenoque@gmail.com>
2026-05-25 17:48:35 +0100

    Tirar o arrendodamento

--- src/module/util/round.ts
+++ src/module/util/round.ts
@@ -1,3 +1,7 @@
 export function roundToInt(value: number): number {
-    return Math.round(value);
+  return Math.round(value);
+}
+
+export function fixToInt(value: number): number {
+  return Number(value.toFixed(2));
 }

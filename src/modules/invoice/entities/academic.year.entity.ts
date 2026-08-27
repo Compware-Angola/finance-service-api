18e6e6f8b3068c8e1b5c187110fd856c9f3a6e50
José Alberto Manuel <96086485+jose005111@users.noreply.github.com>
2026-08-27 12:32:06 +0100

    Refactor/negociacao ano activo (#109)
    * feat: add faseAnoLectivo field to AcademicYear entity and update query to filter by faseAnoLectivo 'ACTIVO'
    
    * feat: enhance NegotiationService and types for improved debt negotiation handling and response structure

--- src/modules/invoice/entities/academic.year.entity.ts
+++ src/modules/invoice/entities/academic.year.entity.ts
@@ -8,7 +8,7 @@ export class AcademicYear {
     name: 'CODIGO',
     type: 'number',
   })
-  codigo: number;
+  codigo!: number;
 
   @Column({
     name: 'DESIGNACAO',
@@ -96,4 +96,11 @@ export class AcademicYear {
     nullable: true,
   })
   codigoTipoCandidatura?: number;
+
+  @Column({
+    name: 'FASE_ANOLECTIVO',
+    type: 'varchar2',
+    nullable: true,
+  })
+  faseAnoLectivo?: string;
 }
